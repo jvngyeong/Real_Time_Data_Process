1002679dcff93c5445b1e07d8118f80c51e55a16
jvngyeong <wnsrud1133@naver.com>
2024-07-08 16:47:24 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Real_Time_Data_Process
+ 실시간 데이터처리 자바 웹서비스 개발과정
---
+++ eclipse-workspace/Day1_1/src/Day1_1/TestClass.java
@@ -0,0 +1,7 @@
+package Day1_1;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("Hello 이숭무");
+	}
+}
